03ebd7c4ba7958ea92a6abb40ef02bac7f3d8f42
unknown <link_2011@126.com>
2024-11-09 13:11:31 +0800

    add mobility update test

--- Assets/Scripts/MobilitySender.cs
+++ Assets/Scripts/MobilitySender.cs
@@ -2,11 +2,10 @@ using System;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
-public class SharedMemorySender : MonoBehaviour
+public class MobilitySender : MonoBehaviour
 {
-    public TxPower TxPower;
-    private const string SharedMemoryName = "Omnetpp_SharedMemoryReceive";
-    private const int BufferSize = 4096; 
+    private const string SharedMemoryName = "Omnetpp_MobilityReceive";
+    private const int BufferSize = 4096;
 
     [DllImport("kernel32.dll", SetLastError = true)]
     private static extern IntPtr CreateFileMapping(IntPtr hFile, IntPtr lpFileMappingAttributes, uint flProtect, uint dwMaximumSizeHigh, uint dwMaximumSizeLow, string lpName);
@@ -20,45 +19,49 @@ public class SharedMemorySender : MonoBehaviour
     [DllImport("kernel32.dll", SetLastError = true)]
     private static extern bool CloseHandle(IntPtr hObject);
 
-    private IntPtr sharedMemoryHandle;
-    private IntPtr sharedMemoryView;
+    private IntPtr mobilityHandle;
+    private IntPtr mobilityView;
 
-    public UeManager UeManager;
 
     void Start()
     {
-        sharedMemoryHandle = CreateFileMapping(new IntPtr(-1), IntPtr.Zero, 0x04, 0, (uint)BufferSize, SharedMemoryName);
-        if (sharedMemoryHandle == IntPtr.Zero)
+        mobilityHandle = CreateFileMapping(new IntPtr(-1), IntPtr.Zero, 0x04, 0, (uint)BufferSize, SharedMemoryName);
+        if (mobilityHandle == IntPtr.Zero)
         {
             Debug.LogError("Failed to create shared memory.");
             return;
         }
 
-        sharedMemoryView = MapViewOfFile(sharedMemoryHandle, 0xF001F, 0, 0, (uint)BufferSize);
-        if (sharedMemoryView == IntPtr.Zero)
+        mobilityView = MapViewOfFile(mobilityHandle, 0xF001F, 0, 0, (uint)BufferSize);
+        if (mobilityView == IntPtr.Zero)
         {
             Debug.LogError("Failed to map view of shared memory.");
-            CloseHandle(sharedMemoryHandle);
+            CloseHandle(mobilityHandle);
             return;
         }
     }
 
     private void Update()
     {
-        
-            SendData(UeManager.ulInfo+UeManager.dlInfo);
-        
+
+        SendData("2049: 370 345 0\n"
+                    +"2050: 50 50 0\n"
+                    +"2051: 30 90 0\n"
+                    +"2052: 150 250 0\n"
+                    +"2053: 250 50 0\n");
+
+
     }
 
     public void SendData(string data)
     {
-        if (sharedMemoryView != IntPtr.Zero)
+        if (mobilityView != IntPtr.Zero)
         {
             byte[] dataBytes = System.Text.Encoding.UTF8.GetBytes(data);
             if (dataBytes.Length < BufferSize)
             {
-                Marshal.Copy(dataBytes, 0, sharedMemoryView, dataBytes.Length);
-                Marshal.WriteByte(sharedMemoryView, dataBytes.Length, 0);
+                Marshal.Copy(dataBytes, 0, mobilityView, dataBytes.Length);
+                Marshal.WriteByte(mobilityView, dataBytes.Length, 0);
             }
             else
             {
@@ -73,15 +76,15 @@ public class SharedMemorySender : MonoBehaviour
 
     void OnApplicationQuit()
     {
-        if (sharedMemoryView != IntPtr.Zero)
+        if (mobilityView != IntPtr.Zero)
         {
-            UnmapViewOfFile(sharedMemoryView);
-            sharedMemoryView = IntPtr.Zero;
+            UnmapViewOfFile(mobilityView);
+            mobilityView = IntPtr.Zero;
         }
-        if (sharedMemoryHandle != IntPtr.Zero)
+        if (mobilityHandle != IntPtr.Zero)
         {
-            CloseHandle(sharedMemoryHandle);
-            sharedMemoryHandle = IntPtr.Zero;
+            CloseHandle(mobilityHandle);
+            mobilityHandle = IntPtr.Zero;
         }
     }
 }
